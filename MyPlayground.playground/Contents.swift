981d5fb8640d14ff22225104877fba42f8a38bdb
kurt ho <ahopdanzer@gmail.com>
2016-08-04 12:37:35 +0800

    first commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+var x = 0
+for i in 0...100 {
+    if i % 2 != 0 {
+        x += i
+    }
+    print(x)
+}
+print(x)
+
+
+
+var y = 0
+for i in 0...10 {
+    if i % 2 != 0 {
+        y += i
+    }
+    print(y)
+}
+
+print(y)
